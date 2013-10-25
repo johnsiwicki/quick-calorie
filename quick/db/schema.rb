76774b9cc7a6370ee5ea5ce29afa6508244484b9
John Siwicki <johnsiwicki@gmail.com>
2013-10-25 13:51:54 -0400

    added photo support

--- quick/db/schema.rb
+++ quick/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131025030500) do
+ActiveRecord::Schema.define(version: 20131025173650) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "kind"
   end
 
 end
